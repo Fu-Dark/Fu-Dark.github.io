48c7cbb9f97890e540c89e18785a815d4004dccc
Fu-Dark <2413725179@qq.com>
2023-01-16 20:28:17 +0800

    Site updated: 2023-01-16 20:28:10

--- js/text.js
+++ js/text.js
@@ -1,24 +1,33 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
-  $("body").click(function(e) {
-    var a = new Array("喜欢我", "不喜欢我");
-    var $i = $("<span/>").text(a[a_idx]);
-    var x = e.pageX,
-    y = e.pageY;
-    $i.css({
-      "z-index": 99999,
-      "top": y - 28,
-      "left": x - a[a_idx].length * 8,
-      "position": "absolute",
-      "color": "#ff7a45"
+    $("body").click(function(e) {
+        var a = new Array
+        ("傲慢","嫉妒","暴怒","怠惰","贪婪","暴食","欲望");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "#696969"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+      3000,
+      function() {
+          $i.remove();
+      });
     });
-    $("body").append($i);
-    $i.animate({
-      "top": y - 180,
-      "opacity": 0
-    }, 1500, function() {
-      $i.remove();
-    });
-    a_idx = (a_idx + 1) % a.length;
-  });
+    setTimeout('delay()', 2000);
 });
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
